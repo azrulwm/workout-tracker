becec32efd0eb1602d44191462fd335e7f9ea44d
azrulwm <azrulazmi.wm@gmail.com>
2022-10-07 00:45:20 +0800

    setting up routes for backennd

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
-const express = require("express");
 require("dotenv").config();
+
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 //express app
 const app = express();
 
 //middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 //routes
-app.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to the app" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 //listen for request
 app.listen(process.env.PORT, () => {
