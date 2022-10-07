2989f6287cac4973f6a14c7ad5e3a3a618340d61
azrulwm <azrulazmi.wm@gmail.com>
2022-10-07 12:05:48 +0800

    adding delete function for both frontend and backend

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,19 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch("api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -12,6 +27,8 @@ const WorkoutDetails = ({ workout }) => {
       </p>
       <p>
         <strong>{workout.createdAt}</strong>
+
+        <span onClick={handleClick}>delete</span>
       </p>
     </div>
   );
