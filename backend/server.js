e63f59e2d4a9ddc5b80130c3f0575f0a6e41ace4
azrulwm <azrulazmi.wm@gmail.com>
2022-10-15 18:42:04 +0800

    updating mongoose.connect parameters

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,10 @@ app.use("/api/user", userRoutes);
 
 //connect to db
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     //listen for request
     app.listen(process.env.PORT, () => {
